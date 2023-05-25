5fac460dc823cab17a290a989e2b3fab397255a0
Ronald Gustavo Nascimento Rodrigues Pereira <ronaald_gustavoo@hotmail.com>
2023-05-24 21:10:35 -0300

    feat: criação da função do cálculo

--- codeInterview/calculo.js
+++ codeInterview/calculo.js
@@ -1,2 +1,12 @@
-var n1 = parsedocument.getElementById('firstNumber').value;;
-var n2 = document.getElementById('secondNumber').value;;
+var n1 = parsedocument.getElementById('firstNumber').value;
+var n2 = document.getElementById('secondNumber').value;
+
+var resultado;
+
+function calcOperation(operation){
+    if (operation ==='adicionar'){
+        resultado = n1 + n2;
+    }
+
+    console.log(resultado)
+}
