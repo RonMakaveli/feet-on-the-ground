a0e33f339e7a19afb3432925e46e9a0e5e8c8c52
Ronald Gustavo Nascimento Rodrigues Pereira <ronaald_gustavoo@hotmail.com>
2023-05-24 21:18:21 -0300

    fix: adicionando o resto das operações

--- codeInterview/calculo.js
+++ codeInterview/calculo.js
@@ -1,11 +1,17 @@
 function calcOperation(operation){
     var n1 = parsedocument.getElementById('firstNumber').value;
     var n2 = document.getElementById('secondNumber').value;
-    
+
     var resultado;
 
-    if (operation ==='adicionar'){
+    if (operation === 'adicionar'){
         resultado = n1 + n2;
+    } else if (operation === 'subtrair') {
+    resultado = n1 - n2;
+    } else if (operation === 'dividir') {
+    resultado = n1 / n2;
+    } else if (operation === 'multiplicar') {
+    resultado = n1 * n2;
     }
 
     console.log(resultado)
