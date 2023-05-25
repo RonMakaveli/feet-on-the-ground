707630167b9931b743f4faa53682e2eeaf8b97cb
Ronald Gustavo Nascimento Rodrigues Pereira <ronaald_gustavoo@hotmail.com>
2023-05-24 21:15:57 -0300

    fix: inserindo as variaveis dentro da função

--- codeInterview/calculo.js
+++ codeInterview/calculo.js
@@ -1,12 +1,17 @@
-var n1 = parsedocument.getElementById('firstNumber').value;
-var n2 = document.getElementById('secondNumber').value;
-
-var resultado;
-
 function calcOperation(operation){
+    var n1 = parsedocument.getElementById('firstNumber').value;
+    var n2 = document.getElementById('secondNumber').value;
+    
+    var resultado;
+
     if (operation ==='adicionar'){
         resultado = n1 + n2;
     }
 
     console.log(resultado)
 }
+
+
+
+
+
