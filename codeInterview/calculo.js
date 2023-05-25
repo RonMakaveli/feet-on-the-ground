22654cff50cb2efc7d189533e03ccb29a8c64e48
Ronald Gustavo Nascimento Rodrigues Pereira <ronaald_gustavoo@hotmail.com>
2023-05-24 21:41:57 -0300

    feat: exibindo resultado para o usuário

--- codeInterview/calculo.js
+++ codeInterview/calculo.js
@@ -14,7 +14,7 @@ function calcOperation(operation){
     resultado = n1 * n2;
     }
 
-    console.log(resultado);
+    document.getElementById('result').innerText = "Resultado: " + resultado;
 }
 
 
