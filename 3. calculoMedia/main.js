e84195ed8e2f6a8d093b203e64ed678e53d0bd2a
Ronald Gustavo <ronaald_gustavoo@hotmail.com>
2023-06-01 11:39:08 -0300

    alterações

--- 3. calculoMedia/main.js
+++ 3. calculoMedia/main.js
@@ -2,16 +2,16 @@ let primeiraNota = document.getElementById("primeiraNota");
 let segundaNota = document.getElementById("segundaNota");
 let terceiraNota = document.getElementById("terceiraNota");
 let quartaNota = document.getElementById("quartaNota");
-let resultadoMedia;
 
 function calcularNota() {
+  let resultadoMedia;
   let nota1 = parseFloat(primeiraNota.value);
   let nota2 = parseFloat(segundaNota.value);
   let nota3 = parseFloat(terceiraNota.value);
   let nota4 = parseFloat(quartaNota.value);
 
   resultadoMedia = (nota1 + nota2 + nota3 + nota4) / 4;
-  console.log(resultadoMedia);
+  document.getElementById('resultadoMedia').innerText = "Resultado: " + resultadoMedia;
 }
 
 calcularNota();
