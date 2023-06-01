0be6d6e014ac2c57b09a8f5c36621922ef2421e1
Ronald Gustavo <ronaald_gustavoo@hotmail.com>
2023-06-01 11:44:31 -0300

    refatoração do código inteiro

--- 3. calculoMedia/main.js
+++ 3. calculoMedia/main.js
@@ -1,17 +1,20 @@
-let primeiraNota = document.getElementById("primeiraNota");
-let segundaNota = document.getElementById("segundaNota");
-let terceiraNota = document.getElementById("terceiraNota");
-let quartaNota = document.getElementById("quartaNota");
-
 function calcularNota() {
+  let primeiraNota = document.getElementById("primeiraNota");
+  let segundaNota = document.getElementById("segundaNota");
+  let terceiraNota = document.getElementById("terceiraNota");
+  let quartaNota = document.getElementById("quartaNota");
   let resultadoMedia;
+
   let nota1 = parseFloat(primeiraNota.value);
   let nota2 = parseFloat(segundaNota.value);
   let nota3 = parseFloat(terceiraNota.value);
   let nota4 = parseFloat(quartaNota.value);
 
   resultadoMedia = (nota1 + nota2 + nota3 + nota4) / 4;
-  document.getElementById('resultadoMedia').innerText = "Resultado: " + resultadoMedia;
-}
 
-calcularNota();
+  if (isNaN(resultadoMedia)) {
+    document.getElementById('resultadoMedia').innerText = "Por favor, insira números válidos.";
+  } else {
+    document.getElementById('resultadoMedia').innerText = "Resultado: " + resultadoMedia;
+  }
+}
