5599d9dc1a0629991aa38abcee46d0782343f8e0
Ronald Rodrigues Pereira <57762848+RonMakaveli@users.noreply.github.com>
2023-12-29 15:13:43 -0300

    Add files via upload

---
+++ 5. ExemploAsp/ExemploAsp/Models/ContatoModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ExemploAsp.Models
+{
+    public class ContatoModel
+    {
+
+        public int MyProperty { get; set; }
+        public string Nome { get; set; }
+        public string CPF { get; set;}
+        public string Email { get; set; }
+
+    }
+}
---
+++ 5. ExemploAsp/ExemploAsp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExemploAsp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
